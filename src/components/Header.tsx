b2fdd724fad47965ab8f64535273d20b72594173
Baskin-Ridley <gbaskinridley@gmail.com>
2022-07-21 11:18:23 +0100

    Fixed SASS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+        <div className="headerLeft">Home</div>
+        <div className="headerRight">
+            <div className="about">
+                About
+            </div>
+            <div className="skills">
+                Skills
+            </div>
+            <div className="projects">
+                Projects
+            </div>
+            <div className="contact">
+                Contact
+            </div>
+        </div>
+    </div>
   )
 }
 
