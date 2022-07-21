52cf39f0dd7672fa537e0b4c4a8d5ad6a69b8a7f
Baskin-Ridley <gbaskinridley@gmail.com>
2022-07-21 14:55:15 +0100

    Fixed margins on webpage to be responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './sass/style.css';
-import Main from './components/Main';
 import Header from './components/Header';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Main/> 
+      <About/> 
     </div>
   );
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import portrait from "../assets/pexels-vincent-tan-6934325.jpg"
+
+const About = () => {
+  return (
+    <div className="aboutContainer">
+        <h2 className="aboutTitle">About</h2>
+        <div className="aboutContent">
+            <p className="aboutText">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet lorem ante. Curabitur tristique fringilla eros. Duis consectetur mauris mi, id congue nisi egestas at. Nunc blandit posuere lectus, nec pulvinar ipsum. Sed felis augue, rutrum a mi nec, sagittis pulvinar dui. Nam et mollis nisi. Nam rutrum, urna in aliquam lacinia, erat sapien dapibus turpis, nec luctus ante lectus eu urna. Donec mattis quis nisl quis pellentesque. Donec sodales euismod lorem eget luctus.
+            </p>
+            <img className="portrait" src={portrait} alt="portrait"/>
+        </div>
+        
+    </div>
+  )
+}
+
+export default About
