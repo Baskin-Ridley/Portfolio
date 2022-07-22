022b0dabe6b1f4ffa8335581ef284c81e5877b81
Baskin-Ridley <gbaskinridley@gmail.com>
2022-07-22 11:13:39 +0100

    Created dummy data file

---
+++ src/database/data.tsx
@@ -0,0 +1,13 @@
+import { getModeForUsageLocation } from "typescript";
+
+export default [
+{
+    name: "Jest",
+    image: "google.com",
+},
+{
+    name: "Javascript",
+    image: "google.com",
+}
+
+]
