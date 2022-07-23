0d24ff95580fd5fe31562e563f195bb85b71c3fd
Baskin-Ridley <gbaskinridley@gmail.com>
2022-07-23 18:17:52 +0100

    Updated Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './sass/style.css';
-
-
+import About from './components/about';
+import Landing from './components/landing';
 function App() {
   return (
     <div className="App">
-
+      <div className="leftContainer">
+        <About />
+      </div>
+      <div className="rightContainer">
+        <Landing/>
+      </div>
     </div>
   );
 }
---
+++ src/components/landing.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import portrait from "../assets/pexels-vincent-tan-6934325534.jpg"
+
+const Landing = () => {
+  return (
+    <img className="portrait" src={portrait} alt="portrait"/>
+  )
+}
+
+export default Landing
