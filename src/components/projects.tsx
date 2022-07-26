e815c7ca950689f59c9231505391467d978e4641
Baskin-Ridley <gbaskinridley@gmail.com>
2022-07-26 14:59:35 +0100

    Finished basic layout of project page

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
         <div className="projectsContainer">
             
                 <h2 className="projectsTitle">Projects</h2>
-                
+                    {/* place holder project */}
                     <div className="project">
                         <div className="projectLeft">
                             <h3 className="projectTitle">Quizzical</h3>
@@ -20,14 +20,50 @@ const Projects = () => {
                             <p className="projectTechnologies">
                                 TypeScript, React, Redux, React-Router, REST API, and Figma
                             </p>
-                            <div className="projectLinks">
-                                <a href="https://baskin-ridley.github.io/Quizzical/">Live App</a>
-                                <a href="https://github.com/Baskin-Ridley/Quizzical">Github</a>
-                            </div>
+                            <a href="https://baskin-ridley.github.io/Quizzical/"><button className="projectLinks">Live App</button></a>
+                            <a href="https://github.com/Baskin-Ridley/Quizzical"><button className="projectLinks">Github</button></a>
                         </div>
                         <img className="projectImage" src={quizzical} alt="Quizzical" />
                     </div>  
-                   
+                    {/* place holder project */}
+                   {/* place holder project */}
+                   <div className="project">
+                        <div className="projectLeft">
+                            <h3 className="projectTitle">Quizzical</h3>
+                            <p className="projectText">
+                            A Quiz game built with React that allows players to answer a series of questions on a subject of their choice, i.e: books or movies. The questions are provided by Open Trivia Database through their REST API.
+                            </p>
+                            <p className="projectText">
+                            I opted to use Redux, as the state manager, and React-Router to make it a multiple page app, as I thought they would be valuable libraries to learn.
+                            </p>
+                            <p className="projectTechnologies">
+                                TypeScript, React, Redux, React-Router, REST API, and Figma
+                            </p>
+                            <a href="https://baskin-ridley.github.io/Quizzical/"><button className="projectLinks">Live App</button></a>
+                            <a href="https://github.com/Baskin-Ridley/Quizzical"><button className="projectLinks">Github</button></a>
+                        </div>
+                        <img className="projectImage" src={quizzical} alt="Quizzical" />
+                    </div>  
+                    {/* place holder project */}
+                    {/* place holder project */}
+                    <div className="project">
+                        <div className="projectLeft">
+                            <h3 className="projectTitle">Quizzical</h3>
+                            <p className="projectText">
+                            A Quiz game built with React that allows players to answer a series of questions on a subject of their choice, i.e: books or movies. The questions are provided by Open Trivia Database through their REST API.
+                            </p>
+                            <p className="projectText">
+                            I opted to use Redux, as the state manager, and React-Router to make it a multiple page app, as I thought they would be valuable libraries to learn.
+                            </p>
+                            <p className="projectTechnologies">
+                                TypeScript, React, Redux, React-Router, REST API, and Figma
+                            </p>
+                            <a href="https://baskin-ridley.github.io/Quizzical/"><button className="projectLinks">Live App</button></a>
+                            <a href="https://github.com/Baskin-Ridley/Quizzical"><button className="projectLinks">Github</button></a>
+                        </div>
+                        <img className="projectImage" src={quizzical} alt="Quizzical" />
+                    </div>  
+                    {/* place holder project */}
                 
            
         </div>
