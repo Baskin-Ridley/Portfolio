1f7e998eb42449390550108a21788343a04f233a
Baskin-Ridley <gbaskinridley@gmail.com>
2022-07-28 11:13:43 +0100

    Added text loading animation

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Main = () => {
   return (
     <div className="mainContainer">
-        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
-        <p> Sed eu eros sit amet turpis semper laoreet. Pellentesque volutpat lectus non sem blandit vestibulum.</p>
+        <h1 className="mainTitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+        <p className="mainParagraph"> Sed eu eros sit amet turpis semper laoreet. Pellentesque volutpat lectus non sem blandit vestibulum.</p>
         <button className="scrollButton">
           Get to know me
         </button>
