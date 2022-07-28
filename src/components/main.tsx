1dd12f2fd8f629284f259574be445afc409e6dba
Baskin-Ridley <gbaskinridley@gmail.com>
2022-07-28 17:55:02 +0100

    Styled the main page

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Main = () => {
   return (
     <div className="mainContainer">
-        <h1 className="mainTitle">Hi, I'm Gabriel, </h1>
-        <p className="mainParagraph"> I'm a self taught-full stack developer. </p>
+        <h1 className="mainTitle">Hi, I'm Gabriel Baskin-Ridley </h1>
+        <p className="mainParagraph"> I am a full-stack developer, using modern frameworks. </p>
         <button className="scrollButton">
           Get to know me!
         </button>
