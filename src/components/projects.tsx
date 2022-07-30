889708576b6f27e7e1ae73752e5d14a2dadfc7c4
Baskin-Ridley <gbaskinridley@gmail.com>
2022-07-30 16:55:42 +0100

    Removed placeholder projects from project

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -26,44 +26,7 @@ const Projects = () => {
                         <img className="projectImage" src={quizzical} alt="Quizzical" />
                     </div>  
                     {/* place holder project */}
-                   {/* place holder project */}
-                   <div className="project">
-                        <div className="projectLeft">
-                            <h3 className="projectTitle">Quizzical</h3>
-                            <p className="projectText">
-                            A Quiz game built with React that allows players to answer a series of questions on a subject of their choice, i.e: books or movies. The questions are provided by Open Trivia Database through their REST API.
-                            </p>
-                            <p className="projectText">
-                            I opted to use Redux, as the state manager, and React-Router to make it a multiple page app, as I thought they would be valuable libraries to learn.
-                            </p>
-                            <p className="projectTechnologies">
-                                TypeScript, React, Redux, React-Router, REST API, and Figma
-                            </p>
-                            <a href="https://baskin-ridley.github.io/Quizzical/"><button className="projectLinks">Live App</button></a>
-                            <a href="https://github.com/Baskin-Ridley/Quizzical"><button className="projectLinks">Github</button></a>
-                        </div>
-                        <img className="projectImage" src={quizzical} alt="Quizzical" />
-                    </div>  
-                    {/* place holder project */}
-                    {/* place holder project */}
-                    <div className="project">
-                        <div className="projectLeft">
-                            <h3 className="projectTitle">Quizzical</h3>
-                            <p className="projectText">
-                            A Quiz game built with React that allows players to answer a series of questions on a subject of their choice, i.e: books or movies. The questions are provided by Open Trivia Database through their REST API.
-                            </p>
-                            <p className="projectText">
-                            I opted to use Redux, as the state manager, and React-Router to make it a multiple page app, as I thought they would be valuable libraries to learn.
-                            </p>
-                            <p className="projectTechnologies">
-                                TypeScript, React, Redux, React-Router, REST API, and Figma
-                            </p>
-                            <a href="https://baskin-ridley.github.io/Quizzical/"><button className="projectLinks">Live App</button></a>
-                            <a href="https://github.com/Baskin-Ridley/Quizzical"><button className="projectLinks">Github</button></a>
-                        </div>
-                        <img className="projectImage" src={quizzical} alt="Quizzical" />
-                    </div>  
-                    {/* place holder project */}
+                   
                 
            
         </div>
