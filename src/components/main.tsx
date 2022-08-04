33e9a7ede70441d3bd8db5664f659dcebd81ec98
Baskin-Ridley <gbaskinridley@gmail.com>
2022-08-04 14:03:02 +0100

    Added links to github, linkedin and resume

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 const Main = ({refToLastComp} : { refToLastComp:any }) => {
   //scroll to element
   
+
   const scrollClick = () => {
     console.log("scroll")
     if (refToLastComp.current) {
@@ -19,9 +20,20 @@ const Main = ({refToLastComp} : { refToLastComp:any }) => {
         <button onClick={scrollClick} className="scrollButton">
           Get to know me!
         </button>
+        <p className="mainLinks">
+
+          <a href="https://github.com/Baskin-Ridley">GitHub</a>, <a href="https://linkedin.com/in/gabriel-edward-baskin-ridley-0732671a0">LinkedIn</a>, and my <a href="/resumes/Gabriel Baskin-Ridley Resume.pdf" download>Resume</a>
+
+        </p>
     </div>
    
   )
 }
+
+
+
+
+
+
 
 export default Main
